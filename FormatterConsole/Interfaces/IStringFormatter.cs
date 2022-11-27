0e282d4233da9cab909e7aef9b4002cedb76107a
MyRainbowDay <abvlex@inbox.ru>
2022-11-27 17:15:02 +0300

    version 1

---
+++ FormatterConsole/Interfaces/IStringFormatter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormatterConsole.Interfaces
+{
+    internal interface IStringFormatter
+    {
+        string Format(string template, params object[] parameters);
+    }
+}
